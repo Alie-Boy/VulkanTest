586e5ae8f9f6338f6d0302f8ffeea77b2efca3eb
CyberBlast <ashishdantesparda@gmail.com>
2020-06-07 18:43:27 +0530

    Setup -- Instance

--- VulkanPrac/main.cpp
+++ VulkanPrac/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication {
 public:
@@ -24,7 +25,7 @@ private:
 	}
 
 	void initVulkan() {
-
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -34,16 +35,58 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
-
 		glfwTerminate();
 	}
+
+
+	void createInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		printf("%s", *glfwExtensions);
+		std::cout << "\n=============================" << std::endl;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+		if (result != VK_SUCCESS) throw std::runtime_error("failed to create instance.");
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		for (const VkExtensionProperties extension : extensions)
+		{
+			std::cout << '\t' << extension.extensionName << '\t' << extension.specVersion << std::endl;
+		}
+	}
+
 
 private:
 	const uint32_t WINDOW_WIDTH = 800;
 	const uint32_t WINDOW_HEIGHT = 600;
 
 	GLFWwindow* window;
+	VkInstance instance;
 };
 
 int main() {
